34daa06e44add4fa98770a6c2bf9fa09ed26bde6
laurent <laurentdelpoux@yahoo.fr>
2017-09-24 06:34:14 +0200

    / ‘2017-09-24_manjasusro/backupconf/backup_files.sh’ + ‘2017-09-24_manjasusro/i3_main’ + ‘2017-09-24_manjasusro/tmuxinator/devcpp.yml’ + ‘2017-09-24_manjasusro/tmuxinator/devphp.yml’ + ‘2017-09-24_manjasusro/tmuxinator/devpy.yml’ + ‘2017-09-24_manjasusro/tmuxinator/html-css-snippets-edit.yml’

--- 2017-09-24_manjasusro/backupconf/backup_files.sh
+++ 2017-09-24_manjasusro/backupconf/backup_files.sh
@@ -6,14 +6,14 @@ cd "/home/laurent/SparkleShare/github.com/conf files"
 mkdir ./$(date -I)_manjasusro
 cd "$(ls -t | head -1)"
 cp -R /home/laurent/programmes/backupconf ./backupconf
-cp -R /home/laurent/.config/i3 ./i3_main
+cp -R /home/laurent/.i3/config ./i3_main
 cp -R /home/laurent/.vim/ftplugin ./.vim_main_ftplugin
 cp -R /home/laurent/.vim/indent ./.vim_main_indent
 cp -R /home/laurent/.vim/UltiSnips ./.vim_main_UltiSnips
-cp -R /home/laurent/.i3blocks.conf ./.i3blocks.conf_main
 cp -R /home/laurent/.config/fish ./fish_main
 cp -R /home/laurent/.config/termite/config ./termiteconf
 cp -R /home/laurent/.tmux.conf ./tmux.conf
+cp -R /home/laurent/.tmuxinator ./tmuxinator
 cp -R /home/laurent/.byobu ./byobu
 cp -R /home/laurent/programmes/.system_me ./
 cp /home/laurent/.xinitrc ./.xinitrc_main
